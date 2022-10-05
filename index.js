9175dfbd395b77b3f6b142cfde6e46b01fb66cb0
Action Jackson <smthbrothrjdev@gmail.com>
2022-10-05 16:27:52 -0500

    added bounding

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ let counter_x = 1;
 let counter_y = 1;
 let prev_ID = 1;
 let selected_ID = 1;
-const TOTAL_ROWS = 30;
-const TOTAL_COLS = 10;
+const TOTAL_ROWS = 10;
+const TOTAL_COLS = 20;
 const BALL_VERTICAL = 30;
 const BALL_HORIZONTAL = 30;
+const container = document.getElementById("container");
 
 //setup maze
 // const ball = document.getElementById("ball");
@@ -22,7 +23,9 @@ document.addEventListener("keydown", (event) => {
   if (event.key == "ArrowUp") move_up();
   if (event.key == "ArrowLeft") move_left();
   if (event.key == "ArrowRight") move_right();
-  paint();
+  if (bumped()) {
+    paint();
+  }
 });
 
 /////////
@@ -33,18 +36,23 @@ function move_down() {
 
 function move_right() {
   selected_ID += 1;
+  if (selected_ID % TOTAL_COLS == 1) {
+    oops();
+  }
 }
 
 function move_left() {
   selected_ID -= 1;
+  if (selected_ID % TOTAL_COLS == 0) {
+    oops();
+  }
 }
 
 function move_up() {
   selected_ID -= TOTAL_COLS;
 }
 
-const container = document.getElementById("container");
-
+///part of init
 function makeRows(rows, cols) {
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
@@ -56,9 +64,23 @@ function makeRows(rows, cols) {
   }
 }
 
-makeRows(TOTAL_ROWS, TOTAL_COLS);
-paint();
+function bumped() {
+  if (selected_ID <= 0 || selected_ID > TOTAL_COLS * TOTAL_ROWS) {
+    oops();
+    return false;
+  } else {
+    if (document.getElementById(selected_ID).style.backgroundColor == "black") {
+      oops();
+    }
+    return true;
+  }
+}
 
+function oops() {
+  console.log("OOPS");
+  selected_ID = prev_ID;
+}
+//paint function
 function paint() {
   document.getElementById(prev_ID).style.backgroundImage = null;
 
@@ -66,3 +88,10 @@ function paint() {
     "url(tennisball.gif)";
   document.getElementById(selected_ID).style.backgroundSize = "100px 100px";
 }
+
+makeRows(TOTAL_ROWS, TOTAL_COLS);
+
+/////paint
+paint();
+
+document.getElementById(8).style.backgroundColor = "black";
